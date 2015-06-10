dbcef803988f10425c382894d1459fab1ac6ad49
Erin Pennington <epennington@utexas.edu>
2015-06-10 00:12:54 -0500

    generated random input for RunCollatz and edited .travis.yml so it performs a diff of the expected to actual output

--- CreateCollatzIn.py
+++ CreateCollatzIn.py
@@ -6,7 +6,7 @@
 
 import sys
 
-from Collatz import cycle_length
+from random import randint
 
 
 # -------------
@@ -26,11 +26,10 @@ def collatz_print_input (w, i, j) :
 # write_sample
 # ------------
 
-def write_sample_in (w) :
+def write_sample (w) :
     """
-    prints original 4 inputs to win and solutions to wout
-    win a writer to input file
-    wout a writer to expected output file
+    prints original 4 inputs to w
+    w a writer to input file
     """
     collatz_print_input(w, 1, 10)
     collatz_print_input(w, 100, 200)
@@ -38,10 +37,47 @@ def write_sample_in (w) :
     collatz_print_input(w, 900, 1000)
 
 # ------------
+# write_random
+# ------------
+
+def write_random (w, n) :
+    """
+    prints n random ranges to w
+    w a writer to input file
+    n the number of ranges to print
+    """
+    for i in range(0, n) :
+        collatz_print_input(w, randint(1,1000000), randint(1,1000000))
+
+# ------------
+# write_random_limited
+# ------------
+
+def write_random_limited (w, n, limit) :
+    """
+    prints n random ranges to w with a range size limit
+    w a writer to input file
+    n the number of ranges to produce
+    limit half of the maximum size of each range produced
+    """
+    for i in range(0, n) :
+        first = randint(1,1000000)
+        second = first + randint(-limit, limit)
+        if second < 1 :
+            second = 1
+        elif second > 1000000 :
+            second = 1000000
+        collatz_print_input(w, first, second)
+
+# ------------
 # main
 # ------------
 
 if __name__ == '__main__':
-    write_sample_in (sys.stdout)
-    
+    write_sample (sys.stdout)
+    write_random(sys.stdout, 10)
+    write_random_limited(sys.stdout, 20, 100000)
+    write_random_limited(sys.stdout, 30, 10000)
+    write_random_limited(sys.stdout, 40, 100)
+    write_random_limited(sys.stdout, 50, 10)
     
